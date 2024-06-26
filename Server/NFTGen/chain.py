343e8f0daecea4aeea49f4d432262b05404f5568
Luca <47627900+Lucaa8@users.noreply.github.com>
2024-06-26 23:40:03 +0200

    Fixed nonce bug

--- Server/NFTGen/chain.py
+++ Server/NFTGen/chain.py
@@ -22,7 +22,7 @@ class CosmicRelic:
             contract_abi = json.load(f)
         assert contract_abi is not None
         self.crel = self.w3.eth.contract(address=cs_contract_address, abi=contract_abi)
-        schedule.every(5).minutes.do(self.check_gas_price)
+        schedule.every(5).seconds.do(self.check_gas_price)
 
         def check_gas_thread():
             while True:
@@ -32,15 +32,16 @@ class CosmicRelic:
 
     def check_gas_price(self):
         from models.ChainTx import ChainTx
-        current_gas_price = self.w3.eth.gas_price
-        for tx in ChainTx.get_transactions():
+        current_gas_price = self.w3.eth.gas_price + self.w3.to_wei(3, 'gwei')
+        for tx in ChainTx.get_all_unsent():
             self.send_tx(tx, current_gas_price)
+            pass
 
     def send_tx(self, tx, gas_price: int):
 
         func_txn = tx.prebuild_tx().build_transaction({
             'from': tx.from_address,
-            'nonce': int(time.time()),
+            'nonce': self.w3.eth.get_transaction_count(tx.from_address),
             'gas': '0',
             'gasPrice': gas_price
         })
@@ -48,14 +49,18 @@ class CosmicRelic:
         gas = self.w3.eth.estimate_gas(func_txn)
         func_txn.update({'gas': gas})
 
-        price_eth = gas * gas_price
+        price_eth = self.w3.from_wei(gas * gas_price, 'ether')
 
         if price_eth < 0.005:
-            signed_txn = self.w3.eth.account.sign_transaction(func_txn, private_key=decrypt_wallet_key(tx.from_pkey))
+            signed_txn = self.w3.eth.account.sign_transaction(func_txn, private_key=self.account.key) #decrypt_wallet_key(tx.from_pkey)
             tx.sent()
             tx_hash = self.w3.eth.send_raw_transaction(signed_txn.rawTransaction)
             print(f'Transaction hash: {self.w3.to_hex(tx_hash)}')
             receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+            # c le receipt ici dessous. Sinon chercher pk sent_at et completed_at ne sont pas mis à jour. Ensuite, actualiser gas et gas_price dans la db aussi.
+            # finalement, voir si c'est possible de lancer les tx en parralèle (si le fait de lancer transaction nonce x, autorise transaction x+1 a s'executer meme si pas encore confirmée
+            # Sinon simplement mettre bool qui dit "si boucle d'il y a 5min pas terminée, ne pas relancer", si ca va devenir le bordel
+            # AttributeDict({'blockHash': HexBytes('0xa2d61f7c12e068efdbd4163b18a584c6ddffd059698ca6c1113ea34895b26308'), 'blockNumber': 6193413, 'contractAddress': None, 'cumulativeGasUsed': 1049429, 'effectiveGasPrice': 3492800329, 'from': '0xF3c5a73fD7E7721C3Bf500b65D4656C4e66A2C98', 'gasUsed': 172722, 'logs': [AttributeDict({'address': '0x4cBCBb04878F75CbD0f0CD9e10613480f2b48Aec', 'topics': [HexBytes('0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'), HexBytes('0x0000000000000000000000000000000000000000000000000000000000000000'), HexBytes('0x000000000000000000000000f3c5a73fd7e7721c3bf500b65d4656c4e66a2c98'), HexBytes('0x0000000000000000000000000000000000000000000000000000000000000000')], 'data': HexBytes('0x'), 'blockNumber': 6193413, 'transactionHash': HexBytes('0xa59039bd44cc0ec16257c74d18f2268224f0f85ae95da7a2df786316049bf539'), 'transactionIndex': 9, 'blockHash': HexBytes('0xa2d61f7c12e068efdbd4163b18a584c6ddffd059698ca6c1113ea34895b26308'), 'logIndex': 16, 'removed': False})], 'logsBloom': HexBytes('0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000200000000000000008000000000002000000000000000000000000000000000000020000000000000000000800000000000000000000000010000000000000000001000000000000000000000000000000000002000000000000000000000000000000000000000040000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000008020000000000000000000000000000000000000000000000000000000000000000000'), 'status': 1, 'to': '0x4cBCBb04878F75CbD0f0CD9e10613480f2b48Aec', 'transactionHash': HexBytes('0xa59039bd44cc0ec16257c74d18f2268224f0f85ae95da7a2df786316049bf539'), 'transactionIndex': 9, 'type': 0})
             tx.completed(receipt)
 
     def check_address(self, address: str) -> bool:
--- Server/NFTGen/models/ChainTx.py
+++ Server/NFTGen/models/ChainTx.py
@@ -2,7 +2,7 @@ import base64
 import json
 from datetime import datetime
 
-from database import db
+from database import db, flask_app
 from sqlalchemy import func, select
 
 
@@ -10,7 +10,7 @@ class ChainTx(db.Model):
     __tablename__ = 'chain_tx'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     from_address = db.Column(db.String(42), nullable=False)
-    from_pkey = db.Column(db.String(42), nullable=False)
+    from_pkey = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, server_default=func.now())
     sent_at = db.Column(db.DateTime, nullable=True)
     completed_at = db.Column(db.DateTime, nullable=True)
@@ -22,7 +22,8 @@ class ChainTx(db.Model):
     def get_all_unsent():
         txs = None
         try:
-            txs = db.session.execute(select(ChainTx).filter_by(sent_at=None)).all()
+            with flask_app.app_context():
+                txs = db.session.execute(select(ChainTx).filter_by(sent_at=None)).all()[0]
         except Exception as e:
             print(f"An unknown error occurred while fetching all unsent transactions: {e}")
         return txs
@@ -34,13 +35,14 @@ class ChainTx(db.Model):
             args.append(arg)
         args = json.dumps(args)
         b64_args: str = base64.b64encode(args.encode()).decode()
-        try:
-            txn = ChainTx(from_address=wallet_addr, from_pkey=wallet_pkey, tx=b64_args)
-            db.session.add(txn)
-            db.session.commit()
-        except Exception as e:
-            db.session.rollback()
-            print(f"An unknown error occurred while registering an new transaction: {e}")
+        with flask_app.app_context():
+            try:
+                txn = ChainTx(from_address=wallet_addr, from_pkey=wallet_pkey, tx=b64_args)
+                db.session.add(txn)
+                db.session.commit()
+            except Exception as e:
+                db.session.rollback()
+                print(f"An unknown error occurred while registering an new transaction: {e}")
 
     def prebuild_tx(self):
         data = json.loads(base64.b64decode(self.tx).decode('utf-8'))
@@ -48,21 +50,23 @@ class ChainTx(db.Model):
         return getattr(cosmic.crel.functions, data[0])(*data[1:])
 
     def sent(self):
-        try:
-            self.sent_at = datetime.now()
-            db.session.commit()
-        except Exception as e:
-            db.session.rollback()
-            print(f"An unknown error occurred while changing sent_at of chain_tx.id=={self.id}: {e}")
+        with flask_app.app_context():
+            try:
+                self.sent_at = datetime.now()
+                db.session.commit()
+            except Exception as e:
+                db.session.rollback()
+                print(f"An unknown error occurred while changing sent_at of chain_tx.id=={self.id}: {e}")
 
     def completed(self, receipt):
         print(receipt)
-        try:
-            self.completed_at = datetime.now()
-            self.gas = 0
-            self.gas_price = 0
-            db.session.commit()
-        except Exception as e:
-            db.session.rollback()
-            print(f"An unknown error occurred while changing completed status of chain_tx.id=={self.id}: {e}")
+        with flask_app.app_context():
+            try:
+                self.completed_at = datetime.now()
+                self.gas = 0
+                self.gas_price = 0
+                db.session.commit()
+            except Exception as e:
+                db.session.rollback()
+                print(f"An unknown error occurred while changing completed status of chain_tx.id=={self.id}: {e}")
 
