c60d967f612d8269186d020f9b4b83fa69e07ef5
Luca <47627900+Lucaa8@users.noreply.github.com>
2024-06-27 16:13:25 +0200

    Improved transaction queue flow

--- Server/NFTGen/chain.py
+++ Server/NFTGen/chain.py
@@ -1,12 +1,14 @@
 import json
 import os
+from datetime import datetime
+
 import schedule
 from threading import Thread
 import time
+
 from utils import decrypt_wallet_key, encrypt_wallet_key
 
 from eth_account import Account
-from eth_account.signers.local import LocalAccount
 from web3 import Web3
 from web3.exceptions import ContractLogicError
 
@@ -22,20 +24,28 @@ class CosmicRelic:
             contract_abi = json.load(f)
         assert contract_abi is not None
         self.crel = self.w3.eth.contract(address=cs_contract_address, abi=contract_abi)
+        self.working = False
         schedule.every(5).seconds.do(self.check_gas_price)
 
         def check_gas_thread():
             while True:
-                schedule.run_pending()
+                if not self.working:
+                    schedule.run_pending()
                 time.sleep(5)
         Thread(target=check_gas_thread).start()
 
     def check_gas_price(self):
+        self.working = True
         from models.ChainTx import ChainTx
         current_gas_price = self.w3.eth.gas_price + self.w3.to_wei(3, 'gwei')
-        for tx in ChainTx.get_all_unsent():
-            self.send_tx(tx, current_gas_price)
-            pass
+        txs = ChainTx.get_all_unsent()
+        print(f"[{datetime.now()}] There are {len(txs)} queued transactions. Current Gas price [Gwei]: {self.w3.from_wei(current_gas_price, 'gwei')}")
+        for tx in txs:
+            try:
+                self.send_tx(tx, current_gas_price)
+            except Exception as e:
+                print(f"Something went wrong while trying to send the transaction chain_tx.id=={tx.id} with the following data: {tx.tx}. Error: {str(e)}")
+        self.working = False
 
     def send_tx(self, tx, gas_price: int):
 
@@ -52,24 +62,24 @@ class CosmicRelic:
         price_eth = self.w3.from_wei(gas * gas_price, 'ether')
 
         if price_eth < 0.005:
-            signed_txn = self.w3.eth.account.sign_transaction(func_txn, private_key=self.account.key) #decrypt_wallet_key(tx.from_pkey)
+            signed_txn = self.w3.eth.account.sign_transaction(func_txn, private_key=decrypt_wallet_key(tx.from_pkey))
             tx.sent()
             tx_hash = self.w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-            print(f'Transaction hash: {self.w3.to_hex(tx_hash)}')
+            print(f"Transaction chain_tx.id=={tx.id} has been sent to the blockchain with hash: {self.w3.to_hex(tx_hash)}. Waiting for confirmation...")
             receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
-            # c le receipt ici dessous. Sinon chercher pk sent_at et completed_at ne sont pas mis à jour. Ensuite, actualiser gas et gas_price dans la db aussi.
-            # finalement, voir si c'est possible de lancer les tx en parralèle (si le fait de lancer transaction nonce x, autorise transaction x+1 a s'executer meme si pas encore confirmée
-            # Sinon simplement mettre bool qui dit "si boucle d'il y a 5min pas terminée, ne pas relancer", si ca va devenir le bordel
-            # AttributeDict({'blockHash': HexBytes('0xa2d61f7c12e068efdbd4163b18a584c6ddffd059698ca6c1113ea34895b26308'), 'blockNumber': 6193413, 'contractAddress': None, 'cumulativeGasUsed': 1049429, 'effectiveGasPrice': 3492800329, 'from': '0xF3c5a73fD7E7721C3Bf500b65D4656C4e66A2C98', 'gasUsed': 172722, 'logs': [AttributeDict({'address': '0x4cBCBb04878F75CbD0f0CD9e10613480f2b48Aec', 'topics': [HexBytes('0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'), HexBytes('0x0000000000000000000000000000000000000000000000000000000000000000'), HexBytes('0x000000000000000000000000f3c5a73fd7e7721c3bf500b65d4656c4e66a2c98'), HexBytes('0x0000000000000000000000000000000000000000000000000000000000000000')], 'data': HexBytes('0x'), 'blockNumber': 6193413, 'transactionHash': HexBytes('0xa59039bd44cc0ec16257c74d18f2268224f0f85ae95da7a2df786316049bf539'), 'transactionIndex': 9, 'blockHash': HexBytes('0xa2d61f7c12e068efdbd4163b18a584c6ddffd059698ca6c1113ea34895b26308'), 'logIndex': 16, 'removed': False})], 'logsBloom': HexBytes('0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000200000000000000008000000000002000000000000000000000000000000000000020000000000000000000800000000000000000000000010000000000000000001000000000000000000000000000000000002000000000000000000000000000000000000000040000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000008020000000000000000000000000000000000000000000000000000000000000000000'), 'status': 1, 'to': '0x4cBCBb04878F75CbD0f0CD9e10613480f2b48Aec', 'transactionHash': HexBytes('0xa59039bd44cc0ec16257c74d18f2268224f0f85ae95da7a2df786316049bf539'), 'transactionIndex': 9, 'type': 0})
-            tx.completed(receipt)
+            tx.completed(receipt, self.w3.from_wei)
 
-    def check_address(self, address: str) -> bool:
-        return self.w3.is_address(address) and self.w3.is_checksum_address(address)
-
-    def mint_nft(self, addr_to: str, uid_to: str, token_id: int, token_type: int):
+    def mint_nft(self, addr_to: str, uid_to: str, token_id: int, token_type: int) -> None:
         from models.ChainTx import ChainTx
         addr_to = self.w3.to_checksum_address(addr_to)
         ChainTx.add_tx(self.account.address, encrypt_wallet_key(self.account.key.hex()), 'mint', (addr_to, uid_to, token_id, token_type))
+
+    def event_mint_successful(self, token_id: int) -> None:
+        # Set NFT to is_minted=1
+        pass
+
+    def check_address(self, address: str) -> bool:
+        return self.w3.is_address(address) and self.w3.is_checksum_address(address)
 
     def get_balance_eth(self, address: str) -> float:
         if not self.check_address(address):
--- Server/NFTGen/models/ChainTx.py
+++ Server/NFTGen/models/ChainTx.py
@@ -15,18 +15,19 @@ class ChainTx(db.Model):
     sent_at = db.Column(db.DateTime, nullable=True)
     completed_at = db.Column(db.DateTime, nullable=True)
     tx = db.Column(db.String(1024), unique=False, nullable=False)
-    gas = db.Column(db.Integer, nullable=True, server_default='0')
-    gas_price = db.Column(db.Integer, nullable=True, server_default='0')
+    gas = db.Column(db.BigInteger, nullable=True, server_default='0')
+    gas_price = db.Column(db.Float, nullable=True, server_default='0.0')
 
     @staticmethod
     def get_all_unsent():
-        txs = None
         try:
             with flask_app.app_context():
-                txs = db.session.execute(select(ChainTx).filter_by(sent_at=None)).all()[0]
+                txs = db.session.execute(select(ChainTx).filter_by(sent_at=None)).all()
+                if txs is not None and len(txs) > 0:
+                    return txs[0]
         except Exception as e:
             print(f"An unknown error occurred while fetching all unsent transactions: {e}")
-        return txs
+        return tuple()
 
     @staticmethod
     def add_tx(wallet_addr: str, wallet_pkey: str, tx_func: str, tx_args: tuple) -> None:
@@ -52,19 +53,25 @@ class ChainTx(db.Model):
     def sent(self):
         with flask_app.app_context():
             try:
-                self.sent_at = datetime.now()
+                tx = db.session.query(ChainTx).filter(ChainTx.id == self.id).first()
+                tx.sent_at = datetime.now()
                 db.session.commit()
             except Exception as e:
                 db.session.rollback()
                 print(f"An unknown error occurred while changing sent_at of chain_tx.id=={self.id}: {e}")
 
-    def completed(self, receipt):
-        print(receipt)
+    def completed(self, receipt, gwei_converter):
+        gas_price_gwei = gwei_converter(receipt['effectiveGasPrice'], 'gwei')
         with flask_app.app_context():
             try:
-                self.completed_at = datetime.now()
-                self.gas = 0
-                self.gas_price = 0
+                tx = db.session.query(ChainTx).filter(ChainTx.id == self.id).first()
+                tx.completed_at = datetime.now()
+                if receipt['status'] == 1: # transaction accepted
+                    tx.gas = receipt['gasUsed']
+                    tx.gas_price = gas_price_gwei
+                    # if mint then call chain#event_mint_successful
+                else:
+                    print(f'Something went wrong with the chain_tx.id=={tx.id}. Check the receipt for more: {receipt}')
                 db.session.commit()
             except Exception as e:
                 db.session.rollback()
